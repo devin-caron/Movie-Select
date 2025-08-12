6d78e67080e8abfb3dd6d4ff1eebe895a5b73d75
devincar <devmcaron@gmail.com>
2025-08-12 14:06:49 -0700

    first commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const movies = {};
+  const handleSearch = () => {};
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-inpuit"
+        />
+        <button type="submit" className="search-btn">
+          Search
+        </button>
+      </form>
+
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
